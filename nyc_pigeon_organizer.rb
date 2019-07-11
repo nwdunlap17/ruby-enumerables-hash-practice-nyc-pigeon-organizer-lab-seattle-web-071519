523d76b6c07207bdea7be6241294b77a20164899
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 21:45:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def look_in_nested_hash(lookup,hash2)
   hash2.each do |hash|
   p "hash: #{hash}"
     hash.each do |name|
-      p "name: #{name[0]}"
+      p "name: #{name}"
       if name[0] == lookup
         p "this hash: #{hash}"
         return hash
